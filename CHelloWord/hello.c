0f41515cc75c7cf2ebda16403ebb6f215df702af
natali <natalihcie@gmail.com>
2018-04-01 19:25:16 -0300

    Hello.c, output.txt and readme.md

---
+++ CHelloWord/hello.c
@@ -0,0 +1,12 @@
+/* hello.c
+ * Programa que imprime por consola: Hello, World!
+ * Natali Huamani Cataño
+ * 1/4/2018
+ */
+
+#include <stdio.h>
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
---
+++ CHelloWord/readme.md
@@ -0,0 +1,7 @@
+Sintaxis y semántica del lenguaje
+TP-0
+Curso: k2051 
+Año: 2018, Anual. 
+Legajo: 1531876 
+Apellido: Huamani Cataño 
+Nombre: Natali
